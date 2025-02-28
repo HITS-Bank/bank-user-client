44dc4f8c652402239aa413360e15fdd485dca204
Серегин Матвей <matvey.s.seregin@gazprombank.ru>
2025-03-01 00:28:47 +0700

    Засетапил Material Theme

--- app/src/main/java/ru/hitsbank/clientbankapplication/presentation/MainActivity.kt
+++ app/src/main/java/ru/hitsbank/clientbankapplication/presentation/MainActivity.kt
@@ -4,25 +4,27 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import ru.hitsbank.clientbankapplication.presentation.theme.ClientBankApplicationTheme
+import ru.hitsbank.clientbankapplication.presentation.theme.AppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            ClientBankApplicationTheme {
+            AppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                        modifier = Modifier.padding(innerPadding),
                     )
                 }
             }
@@ -41,7 +43,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    ClientBankApplicationTheme {
+    AppTheme {
         Greeting("Android")
     }
 }
